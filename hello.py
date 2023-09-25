f6d05d3432379f46890dbfe2c9a38871457b5869
Cynthia E. Alvarez Orbe <cynthialvarezorbe@gmail.com>
2023-09-25 22:10:11 +0000

    add code

--- hello.py
+++ hello.py
@@ -1,1 +1,6 @@
-print('Hello world')
+def add(x, y):
+    return x + y
+
+
+print(add(2, 2))
+# var=
--- myStatements.py
+++ myStatements.py
@@ -7,5 +7,5 @@ for item in my_list:
 for _, value in my_dict.items():
     print(f"My favorite thing to drink is: {value}")
 
-# my_dict=my_dict
-# my_list2=
+#my_dict=my_dict
+#my_list2=
